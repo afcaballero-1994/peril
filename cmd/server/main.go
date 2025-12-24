d220967056de8714d8336b22206b0cf1e1c43628
Andres Pedraza <andrespedraza@Andress-MacBook-Air.local>
2025-12-24 17:59:41 -0500

    feat: added rabbitmq initial connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const connectionString string = "amqp://guest:guest@localhost:5672/"
+	cotion, err := amqp091.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Could no create connection: %v", err)
+	}
+	defer cotion.Close()
 	fmt.Println("Starting Peril server...")
+	log.Println("Connection successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	log.Println("\n Shutting down Connection")
 }
